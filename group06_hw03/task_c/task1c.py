eb9989c7fae53b820a9f236a0a08b1a264a9342a
Gasper Kojek <gasper.kojek@gmail.com>
2016-11-17 00:05:40 +0100

    task c

--- group06_hw03/task_c/task1c.py
+++ group06_hw03/task_c/task1c.py
@@ -1,61 +1,54 @@
-import numpy as np
-import scipy.stats as stats
 import matplotlib.pyplot as plt
 import csv
 import glob
 
-
-
 def main():
-    file_paths = glob.glob("../802_11a/link-*-channel-*.csv")
-    rsss = []
+    # plot_rss()
+    plot_delivery_rates()
 
-    for path in file_paths:
-        rsss.append(get_rss_from_file(path))
-    
+def plot_delivery_rates():
+    links = []
+    channels = []
+    rates = []
+
+    with open("../task_b/aggregated_data_ts_filtered.csv", newline="\n") as csvfile:
+        reader = csv.reader(csvfile, delimiter=',')
+        for row in reader:
+            try:
+                link = int(row[2])
+                channel = int(row[3])
+                rate = float(row[0])
+
+                links.append(link)
+                channels.append(channel)
+                rates.append(rate)
+            except Exception:
+                pass
+
+    plt.title("Link vs Delivery ratio")
+    plt.xlabel("Link")
+    plt.ylabel("Delivery ratio")
+    plt.scatter(links, rates)
+    plt.xticks(links)
+
+    plt.show()
+
+    plt.close()
+
+    plt.title("Channel vs Delivery ratio")
+    plt.xlabel("Channel")
+    plt.ylabel("Delivery ratio")
+    plt.scatter(channels, rates)
+    plt.xticks(channels)
+    plt.show()
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-class Combination(object):
-    name = ""
+def get_ratios_from_file(file):
     link = 0
     channel = 0
-    rss = []
-
-    def __init__(self, link, channel):
-        self.link = link
-        self.channel = channel
-        self.name = "Link %i Channel %i" % (link, channel)
-        self.rss = []
-
-
-def get_rss_from_file(file):
-    filesplit = file.replace("-", ".").split(".")
-
-    data = Combination(int(filesplit[3]), int(filesplit[5]))
-    data.rss.clear()
+    ratios = []
 
     with open(file, newline="\n") as csvfile:
         reader = csv.reader(csvfile, delimiter=',')
@@ -67,7 +60,73 @@ def get_rss_from_file(file):
 
     csvfile.close()
     # print(data.rss)
-    return data
+    return link, channel, ratios
+
+def plot_rss():
+    file_paths = glob.glob("../802_11a/link-*-channel-*.csv")
+    links_rss = {}
+    channels_rss = {}
+
+    for path in file_paths:
+        link, channel, rss = get_rss_from_file(path)
+
+        if link not in links_rss:
+            links_rss[link] = set()
+
+        if channel not in channels_rss:
+            channels_rss[channel] = set()
+
+        links_rss[link].update(rss)
+        channels_rss[channel].update(rss)
+
+    links_plotx = []
+    links_ploty = []
+    for k, v in links_rss.items():
+        for rss in v:
+            links_plotx.append(k)
+            links_ploty.append(rss)
+    plt.title("Link vs RSS")
+    plt.xlabel("Link")
+    plt.ylabel("RSS")
+    plt.scatter(links_plotx, links_ploty)
+    plt.xticks(links_plotx)
+
+    plt.show()
+
+    plt.close()
+
+    plotx = []
+    ploty = []
+    for k, v in channels_rss.items():
+        for rss in v:
+            plotx.append(k)
+            ploty.append(rss)
+    plt.title("Channel vs RSS")
+    plt.xlabel("Channel")
+    plt.ylabel("RSS")
+    plt.scatter(plotx, ploty)
+    plt.xticks(plotx)
+    plt.show()
+
+
+
+def get_rss_from_file(file):
+    filesplit = file.replace("-", ".").split(".")
+    link = int(filesplit[3])
+    channel =  int(filesplit[5])
+
+    rss = []
+
+    with open(file, newline="\n") as csvfile:
+        reader = csv.reader(csvfile, delimiter=',')
+        for row in reader:
+            try:
+                rss.append(int(row[4]))
+            except Exception:
+                pass
+
+    csvfile.close()
+    return link, channel, rss
 
 
 
