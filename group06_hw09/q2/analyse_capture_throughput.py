558792bf961e406e72db341ee16405377bb3a6f1
jens klein <jens.klein@gamebook.io>
2017-01-18 23:49:43 +0100

    Submission ready

--- group06_hw09/q2/analyse_capture_throughput.py
+++ group06_hw09/q2/analyse_capture_throughput.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 import subprocess as sp
 import glob
+import matplotlib.pyplot as plt
 
 TRACES_FOLDER = "traces/"
 
@@ -8,14 +9,52 @@ def main():
   trace_file_paths = glob.glob(TRACES_FOLDER + "trace_*.cap")
 
   #debug
-  trace_file_paths = [trace_file_paths[0]]
+  #trace_file_paths = trace_file_paths[0:1]
 
   filenames = map(lambda fn: fn.split("/")[-1], trace_file_paths)
   meta = map(trace_filename_to_meta, filenames)
   meta = sorted(meta, key=lambda x: x['ts'])
 
-  traces_with_meta = map(read_trace_with_meta, meta)
-  print traces_with_meta
+  traces_with_meta = filter(lambda x: x['throughput'] is not None, map(read_trace_with_meta, meta))
+  grouped_sorted_traces = group_and_sort_traces(traces_with_meta)
+
+  boxplot(grouped_sorted_traces)
+
+
+def boxplot(data_sets):
+  labels = []
+  data = []
+
+  for data_set in data_sets:
+    data.append(data_set['throughputs'])
+    labels.append("Protocol: %s\n TX Rate: %s" % (
+      data_set['meta']['proto'].upper(),
+      data_set['meta']['transmission_rate'].upper()
+    ))
+
+  plt.boxplot(data, notch=True, labels=labels)
+  plt.title('Protocol and Transmission Rate vs Throughput')
+  plt.ylabel('Throughput [Mb/s]')
+  plt.show()
+
+def group_and_sort_traces(traces):
+  grouped_traces_dict = {}
+  for trace in traces:
+    group_key = trace['meta']['transmission_rate'] + trace['meta']['proto']
+    if group_key in grouped_traces_dict.keys():
+      grouped_traces_dict[group_key]['throughputs'].append(trace['throughput'])
+    else:
+      grouped_traces_dict[group_key] = {
+        'throughputs': [trace['throughput']],
+        'meta': {
+          'transmission_rate': trace['meta']['transmission_rate'],
+          'proto': trace['meta']['proto']
+        }
+      }
+  return sorted(
+    grouped_traces_dict.values(), key=lambda x: (x['meta']['proto'], int(x['meta']['transmission_rate'].rstrip('mbps')))
+  )
+
 
 
 def read_trace_with_meta(meta):
@@ -30,17 +69,33 @@ def get_tshark_throughput_in_mbps(file_path, proto):
   tshark_call = ["tshark", "-nr", file_path, "-z", "conv," + proto, "-q"]
 
   # call tcpdump and save output
-  statistics_string = sp.check_output(tshark_call)
+  try:
+    statistics_string = sp.check_output(tshark_call)
+  except sp.CalledProcessError as e:
+    print 'could not process file ' + file_path
+    return None
 
   # keep only the line with the stats and split them by n number of consecutive
   # whitespaces, where n > 0
   stats_line = statistics_string.split("\n")[5].split()
 
+  if len(stats_line) < 7:
+    print stats_line
+    return None
+
   transmitted_bytes = float(stats_line[6])
+  transmitted_frames = int(stats_line[5])
+
+  #calculate the overhead by a given protocol
+  if proto == 'tcp':
+    overhead = transmitted_frames * 128
+  else:
+    overhead = transmitted_frames * 104
+
   duration_in_s = float(stats_line[-1])
 
   ## return throughput
-  return (transmitted_bytes / duration_in_s) / 1000 / 1000
+  return ((transmitted_bytes - overhead )/ duration_in_s) / 1000 / 1000
 
 def trace_filename_to_meta(filename):
   splits = filename.split(".")[0].split("_")
