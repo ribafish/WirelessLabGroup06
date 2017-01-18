58197ad7b7e11c49e9ce31fbd578e6f05b3c83a1
Gasper Kojek <gasper.kojek@gmail.com>
2017-01-18 20:54:07 +0100

    hw09 question 2 c 2nd part script and graph updated for multiple speeds

--- group06_hw09/q2/q2c2.py
+++ group06_hw09/q2/q2c2.py
@@ -4,17 +4,15 @@
 import subprocess as sub
 import sys, getopt
 import matplotlib.pyplot as plt
-import matplotlib.ticker as mtick
+import glob
 import time
 
+
 def main(argv):
-    tcpfile = ""
-    tcptime = 0
-    udpfile = ""
-    udptime = 0
+    files = []
 
     try:
-        opts, args = getopt.getopt(argv, "ht:u:", ["tcpfile=", "udpfile="])
+        opts, args = getopt.getopt(argv, "hi:", ["ifolder="])
     except getopt.GetoptError:
         print('q2c2.py -t <input_tcp> -u <input_udp>')
         sys.exit(2)
@@ -23,46 +21,61 @@ def main(argv):
         if opt == '-h':
             print('q2c2.py -t <input_tcp> -u <input_udp>')
             sys.exit()
-        elif opt in ("-t", "--tcpfile"):
-            tcpfile = arg
-            tcptime = int(arg.split("/")[-1].split("_")[-1].split(".")[0])
-            tcpspeed = arg.split("/")[-1].split("_")[2]
-        elif opt in ("-u", "--udpfile"):
-            udpfile = arg
-            udptime = int(arg.split("/")[-1].split("_")[-1].split(".")[0])
-            udpspeed = arg.split("/")[-1].split("_")[2]
+        elif opt in ("-i", "--ifolder"):
+            folder = arg
 
-    if (len(tcpfile) == 0):
-        raise Exception("TCP file not specified")
-    elif (len(udpfile) == 0):
-        raise Exception("UDP file not specified")
+    if len(folder) == 0:
+        folder = ""
+    elif folder[len(folder) - 1] != "/":
+        folder = folder + "/"
 
-    times_tcp, seqnums_tcp = process_tcp_file(tcpfile)
-    date_tcp = time.strftime('%Y-%m-%d %H:%M', time.localtime(tcptime))
-    times_udp, seqnums_udp = process_udp_file(udpfile)
-    date_udp = time.strftime('%Y-%m-%d %H:%M', time.localtime(udptime))
+    paths = glob.glob("%s*.cap" % folder)
+    paths.sort()
+    files.append(get_first_path(paths, "tcp", "6"))
+    files.append(get_first_path(paths, "tcp", "24"))
+    files.append(get_first_path(paths, "tcp", "54"))
+    files.append(get_first_path(paths, "udp", "6"))
+    files.append(get_first_path(paths, "udp", "24"))
+    files.append(get_first_path(paths, "udp", "54"))
 
-    # print("last 2 UDP seqnums: %i, %i" %(seqnums_udp[-2], seqnums_udp[-1]))
 
-    plt.figure(figsize=(16, 10))
+    #plt.figure(figsize=(25, 15))
 
-    fig, ax1 = plt.subplots()
+    fig, ax1 = plt.subplots(figsize=(12, 10))
     ax1.set_ylabel("TCP Packet sequence number (relative)")
-    tcp, = ax1.plot(times_tcp, seqnums_tcp, label="TCP %s @ %s" % (tcpspeed, date_tcp))
-    ax1.tick_params('y', colors='b')
+    x,y = process_tcp_file(files[0])
+    tcp1, = ax1.plot(x, y, label="TCP %s @ %s" % scrape_speed_date(files[0]), color='b')
+    x,y = process_tcp_file(files[1])
+    tcp2, = ax1.plot(x, y, label="TCP %s @ %s" % scrape_speed_date(files[1]), color='g')
+    x,y = process_tcp_file(files[2])
+    tcp3, = ax1.plot(x, y, label="TCP %s @ %s" % scrape_speed_date(files[2]), color='r')
     ax1.set_xlabel("Delta Time from first packet [s]")
 
     ax2 = ax1.twinx()
-    udp, = ax2.plot(times_udp, seqnums_udp, label="UDP %s @ %s" % (udpspeed, date_udp), color='r')
+    x, y = process_udp_file(files[3])
+    udp1, = ax2.plot(x, y, label="UDP %s @ %s" % scrape_speed_date(files[3]), color='c')
+    x, y = process_udp_file(files[4])
+    udp2, = ax2.plot(x, y, label="UDP %s @ %s" % scrape_speed_date(files[4]), color='m')
+    x, y = process_udp_file(files[5])
+    udp3, = ax2.plot(x, y, label="UDP %s @ %s" % scrape_speed_date(files[5]), color='y')
     ax2.set_ylabel("UDP Packet \"sequence number\"")
-    ax2.tick_params('y', colors='r')
     ax2.yaxis.get_major_formatter().set_powerlimits((0, 2))
 
     plt.title("TCP/UDP packet sequence numbers vs. time")
     plt.grid(True)
-    plt.legend(handles=[tcp, udp], loc="best")
+    plt.legend(handles=[tcp1, tcp2, tcp3, udp1, udp2, udp3], loc="best")
     plt.savefig("tcp_udp_seqnums.png")
 
+def get_first_path(paths, tcpudp, speed):
+    for path in paths:
+        if path.find(tcpudp) != -1 and path.find(speed+"mbps") != -1:
+            return path
+    raise Exception("Cant find a %s file with %smbps" % (tcpudp, speed))
+
+def scrape_speed_date(filepath):
+    date = time.strftime('%Y-%m-%d %H:%M', time.localtime(int(filepath.split("/")[-1].split("_")[-1].split(".")[0])))
+    speed = filepath.split("/")[-1].split("_")[2]
+    return speed, date
 
 def process_tcp_file(filepath):
     tcpdump_call = ("tcpdump -ttttt -r %s ip src 172.17.5.11" % filepath).split(" ")
@@ -93,6 +106,7 @@ def process_tcp_file(filepath):
 
 
 def process_udp_file(filepath):
+    print("Reading from file %s" % filepath)
     tcpdump_call = ("tshark -Y ip.src==172.17.5.11 -r %s "
                     "-T fields -e frame.time_relative -e data.data" % filepath).split(" ")
 
